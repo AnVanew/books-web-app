fd063232679caee30acf43846bfa195853c8bc9b
69AndreevIG <igingerar@gmail.com>
2021-03-16 13:04:39 +0700

    res

--- service/frontend/src/main/java/ru/cbr/study/booksappfrontend/Route/AllBooksView.java
+++ service/frontend/src/main/java/ru/cbr/study/booksappfrontend/Route/AllBooksView.java
@@ -24,6 +24,7 @@ import java.util.List;
 
 @Slf4j
 @Route(value = "books", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
 public class AllBooksView extends AppLayout{
 
     public static final String VIEW_NAME = "Books";
