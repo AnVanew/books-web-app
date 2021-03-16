f0dfabb1365a0fb00d84edbd02e069e4a7de8bad
69AndreevIG <igingerar@gmail.com>
2021-03-16 12:21:19 +0700

    back+front

---
+++ common/src/main/java/ru/cbr/study/book/references/References.java
@@ -0,0 +1,21 @@
+package ru.cbr.study.book.references;
+
+public class References {
+    public static String BOOKS_CONT = "/books";
+    public static String ALL_BOOKS_REF = "/allBooks";
+    public static String BOOK_REF = "/book";
+    public static String DELETE_BOOK_REF = "/deleteBook";
+    public static String AUTHORS_BOOK_REF = "/authorBooks";
+    public static String NAMED_BOOKS_REF = "/namedBooks";
+    public static String ADD_BOOK_REF = "/addBook";
+
+    public static String AUTHORS_CONT = "/authors";
+    public static String ALL_AUTHORS_REF = "/allAuthors";
+    public static String DELETE_AUTHOR_REF = "/deleteAuthor";
+    public static String ADD_AUTHOR_REF = "/addAuthor";
+
+    public static String COMMENTS_CONT = "/comments";
+    public static String ADD_COMMENT_REF = "/addComment";
+    public static String ALL_COMMENTS_REF = "/bookComments";
+
+}
